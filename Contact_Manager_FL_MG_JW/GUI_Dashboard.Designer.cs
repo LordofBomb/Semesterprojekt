7ff01662a360466487ec77a0de071bacc42ea11e
Janik.Wirz <janik.wirz.n@outlook.com>
2025-06-22 18:24:33 +0200

    Test Formsbearbeitung (Dashboard)

--- Contact_Manager_FL_MG_JW/GUI_Dashboard.Designer.cs
+++ Contact_Manager_FL_MG_JW/GUI_Dashboard.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(289, 159);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 0;
+            label1.Text = "TEST";
+            // 
+            // GUI_Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "GUI_Dashboard";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
